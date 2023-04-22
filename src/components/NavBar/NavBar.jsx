c8942871985953c9577b7dc929b40a88f8ecb1fc
Marcos <perezcamposmarcos@gmail.com>
2023-04-22 16:00:57 -0300

    Segunda Entrega

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,17 +12,7 @@ export const Navbar = () => {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="">Inicio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="">Productos</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="">Contacto</a>
-              </li>
-            </ul>
+            
             <div>
             <Categorias />
             </div>
